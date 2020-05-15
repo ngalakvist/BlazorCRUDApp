f7c83dee35e851b3570d67b4265c16e988ed21c3
Ngala Talla <ngala.talla@mdh.se>
2020-05-15 15:09:46 +0200

    Add project files.

---
+++ Data/SqlDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCRUDApp.Data
+{
+  public class SqlDBContext :DbContext
+  {
+    public SqlDBContext(DbContextOptions<SqlDBContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<BlazorCRUDApp.Data.CustomerMaster> CustomerMaster { get; set; }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace BlazorCRUDApp
+{
+  /// <summary>
+  /// Inspiration from
+  /// https://www.codeproject.com/Articles/5253709/All-you-need-to-know-on-Blazor-app-and-create-ASP
+  /// </summary>
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+              webBuilder.UseStartup<Startup>();
+            });
+  }
+}
